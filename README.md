88b9d3d8fefc8e7639878d29f6b6e8e2fa95e146
gaoxiao6331 <gaoxiao6331@163.com>
2024-07-02 23:42:01 +0800

    feat: copy from TKScript

---
+++ README.md
@@ -0,0 +1,7 @@
+# This is a browser extension develpoment template
+# Usage
+```bash
+pnpm i
+```
+# Acknowledgement
+- [TKScript](https://github.com/WindrunnerMax/TKScript)
---
+++ src/bridge/content-inject/request.ts
@@ -0,0 +1,23 @@
+import type { Reflex, Object } from "@/utils/types";
+import { MARK } from "./constant";
+
+const CI_REQUEST_ENUM = ["COPY_TYPE", "KEYBOARD_TYPE", "CONTEXT_MENU_TYPE"] as const;
+export const CONTENT_TO_INJECT_REQUEST = CI_REQUEST_ENUM.reduce(
+  (acc, cur) => ({ ...acc, [cur]: `__${cur}__${MARK}__` }),
+  {} as { [K in typeof CI_REQUEST_ENUM[number]]: `__${K}__${typeof MARK}__` }
+);
+
+export const CI_EXECUTION_ENUM = {
+  START: "START",
+  CLOSE: "CLOSE",
+} as const;
+
+export type CIExecutionType = Object.Values<typeof CI_EXECUTION_ENUM>;
+
+export type EventMap = {
+  [CONTENT_TO_INJECT_REQUEST.COPY_TYPE]: CIExecutionType;
+  [CONTENT_TO_INJECT_REQUEST.KEYBOARD_TYPE]: CIExecutionType;
+  [CONTENT_TO_INJECT_REQUEST.CONTEXT_MENU_TYPE]: CIExecutionType;
+};
+
+export type CIRequestType = Reflex.Tuple<EventMap>;
